8cc097d97457139f34c839f8ec07500c9ee78e52
Buch, Amisha Amit <mishabuch@gatech.edu>
2020-07-26 19:37:05 +0530

    Update CartServiceTest.java

--- Online-Grocery-Store/src/test/java/com/javapoint/service/test/CartServiceTest.java
+++ Online-Grocery-Store/src/test/java/com/javapoint/service/test/CartServiceTest.java
@@ -8,12 +8,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import com.javapoint.enums.ErrorCodeEnums;
 import com.javatpoint.model.Cart;
-import com.javatpoint.model.Order;
 import com.javatpoint.model.OrderProduct;
 import com.javatpoint.model.User;
 import com.javatpoint.repository.CartRepository;
@@ -21,7 +18,6 @@ import com.javatpoint.repository.OrderProductRepository;
 import com.javatpoint.repository.OrderRepository;
 import com.javatpoint.repository.UserRepository;
 import com.javatpoint.service.CartService;
-import com.javatpoint.service.OrderService;
 import com.javatpoint.service.ProductService;
 import com.javatpoint.service.UserService;
 
@@ -54,13 +50,6 @@ public class CartServiceTest {
 
 		cartService = new CartService();
 		MockitoAnnotations.initMocks(this);
-		//Mockito.doReturn(createValidCart()).when(cartService).getCart(Mockito.anyObject());
-		//Mockito.doNothing().doThrow(new RuntimeException()).when(productService).decreaseStock(Mockito.anyInt(), Mockito.anyInt());
-		//Mockito.doNothing().doThrow(new RuntimeException()).when(orderProductRepository).save(Mockito.any(OrderProduct.class));
-		//Mockito.doNothing().doThrow(new RuntimeException()).when(orderProductRepository).delete(Mockito.anyInt());
-		//Mockito.doNothing().doThrow(new RuntimeException()).when(orderRepository).save(Mockito.any(Order.class));
-		//Mockito.doNothing().doThrow(new RuntimeException()).when(cartRepository).save(Mockito.any(Cart.class));
-
 }
 
 	@Test
@@ -68,22 +57,16 @@ public class CartServiceTest {
 		Cart cart = cartService.getCart(createValidUser());
 		Assert.assertEquals(cart.getCartId(), 1);
 	}
-
-	private Cart createValidCart() {
-		Cart cart = new Cart();
-		cart.setCartId(1);
-		Set<OrderProduct> productsInOrder = new HashSet<>();
-		OrderProduct op = new OrderProduct();
-		op.setCount(1);
-		op.setId(1);
-		op.setProductId(1);
-		op.setProductName("tea");
-		op.setProductPrice(100);
-		productsInOrder.add(op);
-		cart.setProductsInOrder(productsInOrder);
-		return cart;
-	}
 	
+	@Test
+	public void testPositiveCheckoutOrder() {
+		try {
+			cartService.checkout(createInValidUser());
+		} catch (Exception e) {
+			Assert.assertEquals(ErrorCodeEnums.CART_IS_EMPTY.getMessage(), e.getMessage());
+		}
+		
+	}
 	private User createValidUser() {
 		User user = new User();
 		user.setUserName("User1");
@@ -105,6 +88,33 @@ public class CartServiceTest {
 		op.setProductPrice(100);
 		productsInOrder.add(op);
 		cart.setProductsInOrder(productsInOrder);
+		
+		user.setCart(cart);
+		return user;
+
+	}
+	
+	private User createInValidUser() {
+		User user = new User();
+		user.setUserName("User1");
+		user.setUserId(1);
+		user.setPhone(9663507);
+		user.setPassword("password");
+		user.setIsAdmin(1);
+		user.setEmail("user1@gmail.com");
+		user.setAddress("India");
+		
+		Cart cart = new Cart();
+		cart.setCartId(1);
+		Set<OrderProduct> productsInOrder = new HashSet<>();
+		OrderProduct op = new OrderProduct();
+		op.setCount(1);
+		op.setId(1);
+		op.setProductId(1);
+		op.setProductName("tea");
+		op.setProductPrice(100);
+		productsInOrder.add(op);
+		//cart.setProductsInOrder(productsInOrder);
 		
 		user.setCart(cart);
 		return user;
